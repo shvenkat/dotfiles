e75f85824f4efc38c08f610637513a33c4b0f5a9
Shiv <shvenkat@users.noreply.github.com>
2015-08-26 15:12:32 -0700

    Squashed '.vim/bundle/color_coded/' content from commit 2d068ff
    git-subtree-dir: .vim/bundle/color_coded
    git-subtree-split: 2d068ffea23d1ac88756a128677a2d29d56737ee

---
+++ include/async/queue.hpp
@@ -0,0 +1,93 @@
+#pragma once
+
+#include <map>
+#include <thread>
+#include <future>
+#include <atomic>
+#include <chrono>
+#include <mutex>
+#include <condition_variable>
+
+namespace color_coded
+{
+  namespace async
+  {
+    template <typename Task, typename Result>
+    class queue
+    {
+      public:
+        using func_t = std::function<Result (Task const &)>;
+
+        queue() = delete;
+        queue(func_t &&f)
+          : func_{ std::move(f) }
+          , thread_{ std::bind(&queue::work, this) }
+        { }
+
+        void push(Task &&t)
+        {
+          {
+            std::lock_guard<std::mutex> const lock{ task_mutex_ };
+            task_ = std::move(t);
+            has_work_.store(true);
+          }
+          task_cv_.notify_one();
+        }
+        std::pair<Result, bool> pull(std::string const &name)
+        {
+          std::lock_guard<std::mutex> const lock{ results_mutex_ };
+          return { std::move(results_[name]), has_results_[name].exchange(false) };
+        }
+
+        void join()
+        {
+          should_work_.store(false);
+          has_work_.store(true);
+          task_cv_.notify_one();
+          thread_.join();
+        }
+
+      private:
+        void work()
+        {
+          Task curr;
+          Result result;
+          while(should_work_.load())
+          {
+            {
+              std::unique_lock<std::mutex> lock{ task_mutex_ };
+              task_cv_.wait(lock, [&]{ return has_work_.load(); });
+
+              /* We may be woken up to die. */
+              if(!should_work_.load())
+              { return; }
+
+              curr = std::move(task_);
+              has_work_.store(false);
+            }
+
+            result = func_(curr);
+
+            {
+              std::lock_guard<std::mutex> const lock{ results_mutex_ };
+              has_results_[result.name].store(true);
+              results_[result.name] = std::move(result);
+            }
+          }
+        }
+
+        func_t const func_;
+        std::thread thread_;
+        std::atomic_bool should_work_{ true };
+
+        std::atomic_bool has_work_{};
+        Task task_;
+        std::mutex task_mutex_;
+        std::condition_variable task_cv_;
+
+        std::map<std::string, std::atomic_bool> has_results_{};
+        std::map<std::string, Result> results_;
+        std::mutex results_mutex_;
+    };
+  }
+}
---
+++ include/events.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "core.hpp"
+
+namespace color_coded
+{
+  namespace event
+  {
+    inline bool pull(std::string const &file)
+    {
+      auto const pulled(core::queue().pull(file));
+      if(pulled.second)
+      {
+        auto &buf(core::buffers()[pulled.first.name]);
+        buf.group = std::move(pulled.first.group);
+        return file == pulled.first.name;
+      }
+      return false;
+    }
+
+    inline void push(std::string const &file, std::string const &data)
+    {
+      if(pull(file))
+      { vim::apply(core::buffers()[file]); }
+      core::queue().push({ file, data });
+    }
+
+    inline void moved(std::string const &file, std::size_t const begin,
+                      std::size_t const end)
+    {
+      auto &buf(core::buffers()[file]);
+      buf.new_begin = begin;
+      buf.new_end = end;
+
+      if(pull(file)) /* Pulled new info. */
+      { vim::apply(buf); }
+      else /* See if the move requires another application. */
+      { vim::try_apply(buf); }
+    }
+
+    inline void enter(std::string const &file, std::string const &data)
+    {
+      auto &buf(core::buffers()[file]);
+      if(buf.group.size())
+      { vim::apply(buf); }
+      core::queue().push({ file, data });
+    }
+
+    inline void destroy(std::string const &file)
+    { core::buffers().erase(file); }
+
+    inline void exit()
+    { core::queue().join(); }
+
+    inline std::string last_error()
+    { return core::last_error(); }
+  }
+}
