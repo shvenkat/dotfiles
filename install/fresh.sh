96e270b5561c6db9aa279d6951f604a3dfc6d787
Shiv <shvenkat@users.noreply.github.com>
2017-03-03 14:13:56 -0800

    WIP: untested config bootstrap script.

--- install/fresh.sh
+++ install/fresh.sh
@@ -11,22 +11,46 @@ FRESHRC_DST="${HOME}/.freshrc"
 NAME="$(basename "$0")"
 
 
-# Write to stderr.
-stderr () {
-    echo "$@" 1>&2
+# Initialize logging by setting global variables.
+init_logger () {
+    if [ -t 1 ]; then
+        NORMAL="\e[0m"
+        RED="\e[31m"
+        YELLOW="\e[33m"
+    else
+        NORMAL=""
+        RED=""
+        YELLOW=""
+    fi
 }
 
 
-# Write formatted error messages to stderr.
+# Output formatted error message and exit.
 error () {
-    stderr "ERROR	[${NAME}]$*."
+    env echo -e "${RED}ERROR${NORMAL}  [$NAME]  $*."
     exit 1
 }
 
 
-# Write formatted error messages to stderr.
+# Output formatted warning messages.
+warn () {
+    env echo -e "${YELLOW}WARN${NORMAL}   [$NAME]  $*."
+}
+
+
+# Output formatted informational messages.
 info () {
-    echo "[${NAME}]	INFO	$*."
+    env echo -e "INFO   [$NAME]  $*."
+}
+
+
+# Run a command, logging its output. If an error is encountered, output an error message and exit.
+# Args:
+#   $@: Command and arguments to be run.
+run () {
+    info "Running command $* (in $(pwd))."
+    ("$@" 2>&1 | while IFS='' read -r line; do echo "        $line"; done) \
+        || error "$1 command failed."
 }
 
 
@@ -37,64 +61,68 @@ info () {
 #   $2: Local path.
 clone_safely () {
     if [ ! -e "$2" ]; then
-        echo
-        git clone "$1" "$2"
+        run git clone "$1" "$2"
     else
+        # Attempt to replicate the effect of 'git clone' in a non-destructive way. To do this,
+        # verify or satisfy the following assertions and fail otherwise.
+        # * The local path is a git repo, in a clean state with no untracked files.
+        # * The git repo has a remote named 'origin' matching the given URL.
+        # * The git repo has a branch that tracks the origin branch referred to by origin/HEAD.
+        # Checkout and update (fetch and merge) the appropriate local branch.
+
         # Verify that the local path is a directory.
         test -d "$2" \
-            || error "Cannot clone $1; incompatible file type exists at $2."
+            || error "Cannot clone $1; incompatible file type (non-directory) exists at $2."
         current_dir="$(pwd)"
         cd "$2"
-        # Temporarily suppress output while checking whether the existing directory.
-        exec
-
-        # Verify that the local path is a git repo, in a clean state with no untracked files, has
-        # an origin matching the given remote URL and a master branch tracking the remote master. If
-        # so, update it. Otherwise, complain and exit.
-
         # Verify that the local path is a git repository.
         git rev-parse --is-inside-work-tree >/dev/null 2>&1 \
-            || error "Cannot clone $1; existing directory at $2 is not a git repository."
+            || error "Cannot clone $1 to $2; existing directory is not a git repository."
         # Verify that the working tree is clean regarding tracked files.
         git diff-index --quiet HEAD >/dev/null 2>&1 \
-            || error "Cannot clone $1; existing git repo at $2 is not clean."
+            || error "Cannot clone $1 to $2; existing git repo is not clean."
         # Verify that there are no untracked files. These could conflict with a pull.
         test -z "$(git ls-files --others --exclude-standard 2>/dev/null)" \
-            || error "Cannot clone $1; existing git repo at $2 has untracked files."
+            || error "Cannot clone $1 to $2; existing git repo has untracked files."
         # Verify that a remote named 'origin' exists and has the given URL.
         if (git remote | grep -q '^origin$') >/dev/null 2>&1; then
             # Verify that the URL of the remote 'origin' matches the given one.
             test "$1" = "$(git ls-remote --get-url origin 2>/dev/null)" \
-                || error "Cannot clone $1; existing git repo at $2 has a different 'origin' remote."
+                || error "Cannot clone $1 to $2; existing git repo has a different 'origin' remote."
         else
             # Add an 'origin' remote.
-            git remote add origin "$1"
+            run git remote add origin "$1"
         fi
         # Determine the default remote branch i.e. resolve origin/HEAD. Typically 'master'.
-        origin_head="$(git ls-remote --symref origin HEAD | grep -o 'refs/heads/[^[:space:]]\+')"
+        origin_head="$(git ls-remote --symref origin HEAD \
+                        | grep -o 'refs/heads/[^[:space:]]\+')" 2>/dev/null \
+            || error "Cannot clone $1 to $2; unable to resolve origin/HEAD for existing git repo."
         origin_head="${origin_head#refs/heads/}"
-        # Ensure that a local tracking branch exists for the default remote branch. Find its name if
-        # it exists or create one.
+        # Ensure that a local tracking branch exists for the default remote branch. Identify such a
+        # local branch or create one.
         tracking_heads="$(git for-each-ref --format='%(refname) %(upstream)' refs/heads \
                             | (grep " refs/remotes/origin/$origin_head" || true) \
                             | cut -d' ' -f1 \
-                            | sed -e 's#refs/heads/##')"
+                            | sed -e 's#refs/heads/##')" 2>/dev/null
         if [ -z "$tracking_heads" ]; then
             # Verify the absence of a local branch with the same name as origin/HEAD.
-            git show-ref --verify --quiet "refs/heads/$origin_head" \
-                || error "Cannot clone $1; branch $origin_head in the existing git repo at $2" \
-                    "does not track origin/$origin_head (the current origin/HEAD)."
+            git show-ref --verify --quiet "refs/heads/$origin_head" 2>/dev/null \
+                && error "Cannot clone $1 to $2; existing git repo has a $origin_head branch " \
+                    "that does not track origin/$origin_head (the current origin/HEAD)."
             # Create a local branch that tracks origin/HEAD.
-            git checkout -b "$origin_head" --track "origin/$origin_head"
+            run git checkout -b "$origin_head" --track "origin/$origin_head"
         else
-            # If there is a single matching local branch, use it. If there are multiple, select one
-            # arbitrarily.
-            local_head="$(echo "$tracking_heads" | head -n1)"
-            git checkout "$local_head"
+            # If there is a single matching local branch, use it. If there are multiple, use the one
+            # with the same name as the remote branch. Otherwise, select one arbitrarily.
+            if echo "$tracking_heads" | grep -q "^${origin_head}$"; then
+                local_head="${origin_head}"
+            else
+                local_head="$(echo "$tracking_heads" | head -n1)" 2>/dev/null
+            fi
+            run git checkout "$local_head"
         fi
         # Fetch and merge.
-        git pull
-
+        run git pull origin "$origin_head"
         cd "$current_dir"
     fi
 }
@@ -110,6 +138,7 @@ symlink_safely () {
         echo "Warning: $FRESHRC_DST exists, aborting."
         exit 1
     fi
+    run ln -sf "$1" "$2"
 }
 
 
