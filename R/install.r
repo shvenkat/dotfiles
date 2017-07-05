a5a0116baa06fa87911f1d9869969a67794e717e
Shiv <shvenkat@users.noreply.github.com>
2017-07-04 17:25:27 -0700

    Add bare-bones R package installer, along the lines of Python pip.

--- R/install.r
+++
@@ -1,8 +0,0 @@
-suppressMessages(require("devtools")) || install.packages("devtools")
-suppressMessages(require("ggplot2"))  || install.packages("ggplot2")
-suppressMessages(require("plyr"))     || install.packages("plyr")
-if (!suppressMessages(require("foo"))) {
-    tryCatch(install.packages("foo"), warning = function(w) stopifnot(FALSE))
-}
-# install.packages(c('repr', 'IRdisplay', 'evaluate', 'crayon', 'pbdZMQ', 'uuid', 'digest'))
-# devtools::install_github('IRkernel/IRkernel')
