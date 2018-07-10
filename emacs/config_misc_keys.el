93b945cc6b3b3993ab8c65f06ef3874c67ee4245
Shiv <shvenkat@users.noreply.github.com>
2018-07-10 11:41:25 -0700

    Fix iTerm2 profile installer. Minor updates to config and packages.

--- emacs/config_misc_keys.el
+++ emacs/config_misc_keys.el
@@ -3,4 +3,8 @@
     (progn
         (evil-leader/set-key "s" 'mode-line-other-buffer)
         (evil-leader/set-key "h" 'revert-buffer)
-        (evil-leader/set-key "w" '(lambda () (interactive) (evil-window-set-width (+ 2 fill-column))))))
+        (evil-leader/set-key "w" '(lambda () (interactive)
+                                      (evil-window-set-width fill-column)
+                                      (set-window-margins (selected-window) 1 1)
+                                      (set-window-fringes (selected-window) 0 0)
+                                      (set-window-scroll-bars (selected-window) 0 nil 0 nil)))))
