e37d8df9bedb8140205cc0e5013748b8b85c4f71
Shiv <shvenkat@users.noreply.github.com>
2023-09-11 18:59:51 -0700

    Minor update.

--- emacs/custom.el
+++ emacs/custom.el
@@ -6,3 +6,6 @@
 ;; https://github.com/brainlessdeveloper/emacs.d/
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file)
+
+;; Keep a few context lines above and below the cursor when scrolling.
+(setq scroll-margin 3)
