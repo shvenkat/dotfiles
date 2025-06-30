c3d0b286b5421754afd40d638babe4841dc08a66
Shiv <shvenkat@users.noreply.github.com>
2025-06-30 11:02:13 -0700

    Emacs: auto-reload changed files. Shell: setup xsv aliases.

--- emacs/custom.el
+++ emacs/custom.el
@@ -9,3 +9,9 @@
 
 ;; Keep a few context lines above and below the cursor when scrolling.
 (setq scroll-margin 3)
+
+;; Reload buffers that have changed on disk, if there are no unsaved changes.
+(global-auto-revert-mode 1)
+(setq auto-revert-use-notify t)
+(setq auto-revert-interval 5)
+(setq auto-revert-remote-files nil)
