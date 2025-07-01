2bd0a047bf95a033e76635fada08cd7fafd5e80d
Shiv <shvenkat@users.noreply.github.com>
2025-06-30 22:25:22 -0700

    Fix magit config.

--- emacs/magit.el
+++ emacs/magit.el
@@ -5,8 +5,15 @@
 ;; Use evil for vim-like keybindings.
 (use-package magit
     :init
+    (add-hook 'magit-mode-hook
+        #'(lambda ()
+              (setq-local fill-column 110)
+              (setq-local display-line-numbers nil)))
     ;; Don't refresh the git status buffer unless it is active.
     (setq magit-refresh-status-buffer nil)
+    (setq magit-display-buffer-function
+        #'magit-display-buffer-fullframe-status-topleft-v1)
+    (setq magit-diff-refine-hunk t)
     ;; Magit WIP mode adds a noticeable lag when saving file buffers.
     ;; (magit-wip-mode)
     )
