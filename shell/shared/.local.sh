1b0759f528f9a1df5274a762aede3b6a171486fd
Shiv <shvenkat@users.noreply.github.com>
2025-08-10 10:09:59 -0700

    Add timeout shell function. Update shell config.

--- shell/shared/.local.sh
+++ shell/shared/.local.sh
@@ -19,29 +19,3 @@ fi
 alias diff_something="rsync -n -aOzi --delete \
     --exclude ... --include '/foo/*.bar' --exclude /foo/ \
     /local/path/ machine:/remote/path/"
-
-
-# ----------  Completion  ----------------------------------------------------
-
-if [[ -n "$ZSH_VERSION" ]]; then
-    autoload bashcompinit
-    bashcompinit
-fi
-
-
-# ----------  Python virtualenv  ---------------------------------------------
-
-if [[ -n "$BASH_VERSION" && -n "$PS1" && "$TERM" != 'dumb' ]] && tty -s; then
-    # Updates $PS1.
-    __update_prompt () {
-        if [[ -n "$VIRTUAL_ENV" ]]; then
-            prompt_color="\[\033[91m\]"
-        else
-            prompt_color="\[\033[30m\]"
-        fi
-        PS1="${HOSTNAME:0:4} $(__shrink_path "$(pwd)") ${prompt_color}◀▶\[\033[0m\] "
-        unset prompt_color
-    }
-    # Set the prompt now.
-    __update_prompt
-fi
